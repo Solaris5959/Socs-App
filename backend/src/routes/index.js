83f9eb06d7d47fd9ab75e29bd5bc65f07d0fa6b8
audreylearns <audreyduzon.cda@gmail.com>
2025-05-23 19:47:31 -0400

    feat/auth(backend): protected routes set up via AccessToken

---
+++ backend/src/routes/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+//mount all api end-point here
+const router = express.Router();
+import { test } from './test.js'; // Importing the method test using ESM syntax
+
+
+//define all routes here ('/route' , method) all with have the req.user object
+//front end calls "http://localhost:8080/api/route_name"
+router.get('/test_auth', test); //sample route to follow
+
+export default router; //export for server.js
