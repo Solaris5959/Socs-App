a1776ad04ddddd5d9031fc8c2139d0bcd445c581
Raphael Antioquia <raphael.antioquia@gmail.com>
2025-05-29 17:14:40 -0400

    Refactor dashboard route to be under api/v1/

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import dashboardRoutes from './dashboard.js';
 
 //mount all api end-point here
 const router = express.Router();
@@ -8,9 +7,5 @@ import { test } from './test.js'; // Importing the method test using ESM syntax
 //define all routes here ('/route' , method) all with have the req.user object
 //front end calls "http://localhost:8080/api/route_name"
 router.get('/test_auth', test); //sample route to follow
-
-// new dashboard sub‐router
-router.use('/dashboard', dashboardRoutes);
-
 
 export default router; //export for server.js
