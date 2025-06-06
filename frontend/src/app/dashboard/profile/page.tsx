e65035b6c4de7cf47ddd56709b2c744cf27446c6
Supachai Ruknuy <supachai5028@gmail.com>
2025-06-06 15:35:58 -0400

    updated site-bar

--- frontend/src/app/dashboard/profile/page.tsx
+++ frontend/src/app/dashboard/profile/page.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import { UserProfile } from '@/components/UserProfile'
 
+import { Metadata } from "next"
+export const metadata: Metadata = {
+    title: "User Profile | SOCS",
+    description: "View and manage your profile settings, including personal information, account details, and preferences.",
+}
+
+
 // This is the user profile page component
 export default function page() {
     return (
--- frontend/src/components/nav-user.tsx
+++ frontend/src/components/nav-user.tsx
@@ -32,6 +32,8 @@ import {
 import { useAuth } from '../../contexts/AuthContext';
 import { useRouter } from "next/navigation"
 
+// Use context to access the sidebar state
+import { useProfile } from '../../contexts/UserContext';
 
 // User sidebar component
 export function NavUser({
@@ -47,46 +49,38 @@ export function NavUser({
   const { signOut } = useAuth();
   const router = useRouter()
 
-  // Function to handle sign out
-  const handleSignOut = async () => {
-    try {
-      await signOut();
-      // Redirect to the login page or perform any other action after sign out
-      router.push("/");
-    } catch (error) {
-      console.error("Error signing out:", error);
-      // Handle error (e.g., show a notification)
+  // Use the UserContext to get user profile data and methods
+  const { userProfile } = useProfile();
+
+  // Assign user profile data to user variable
+  if (useProfile) {
+    user = {
+      name: userProfile?.first_name || user.name,
+      email: userProfile?.email || user.email,
+      avatar: userProfile?.profile_pic_url || user.avatar,
     }
-  }
-  return (
-    <SidebarMenu>
-      <SidebarMenuItem>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <SidebarMenuButton
-              size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-            >
-              <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src="" alt={user.name} />
-                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
-              </Avatar>
-              <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-medium">{user.name}</span>
-                <span className="truncate text-xs">{user.email}</span>
-              </div>
-              <ChevronsUpDown className="ml-auto size-4" />
-            </SidebarMenuButton>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent
-            className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
-            side={isMobile ? "bottom" : "right"}
-            align="end"
-            sideOffset={4}
-          >
-            <DropdownMenuLabel className="p-0 font-normal">
-              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                {/* User Profile Detail */}
+
+
+    // Function to handle sign out
+    const handleSignOut = async () => {
+      try {
+        await signOut();
+        // Redirect to the login page or perform any other action after sign out
+        router.push("/");
+      } catch (error) {
+        console.error("Error signing out:", error);
+        // Handle error (e.g., show a notification)
+      }
+    }
+    return (
+      <SidebarMenu>
+        <SidebarMenuItem>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <SidebarMenuButton
+                size="lg"
+                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+              >
                 <Avatar className="h-8 w-8 rounded-lg">
                   <AvatarImage src={user.avatar} alt={user.name} />
                   <AvatarFallback className="rounded-lg">CN</AvatarFallback>
@@ -95,52 +89,72 @@ export function NavUser({
                   <span className="truncate font-medium">{user.name}</span>
                   <span className="truncate text-xs">{user.email}</span>
                 </div>
-              </div>
-            </DropdownMenuLabel>
+                <ChevronsUpDown className="ml-auto size-4" />
+              </SidebarMenuButton>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+              side={isMobile ? "bottom" : "right"}
+              align="end"
+              sideOffset={4}
+            >
+              <DropdownMenuLabel className="p-0 font-normal">
+                <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                  {/* User Profile Detail */}
+                  <Avatar className="h-8 w-8 rounded-lg">
+                    <AvatarImage src={user.avatar} alt={user.name} />
+                    <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                  </Avatar>
+                  <div className="grid flex-1 text-left text-sm leading-tight">
+                    <span className="truncate font-medium">{user.name}</span>
+                    <span className="truncate text-xs">{user.email}</span>
+                  </div>
+                </div>
+              </DropdownMenuLabel>
 
-            {/* User menu */}
-            <DropdownMenuGroup>
+              {/* User menu */}
+              <DropdownMenuGroup>
 
-              {/* Profile Page*/}
-              <a href="/dashboard/profile"  >
-                <DropdownMenuItem className="cursor-pointer" >
-                  <CircleUserRound />
-                  Profile
+                {/* Profile Page*/}
+                <a href="/dashboard/profile"  >
+                  <DropdownMenuItem className="cursor-pointer" >
+                    <CircleUserRound />
+                    Profile
+                  </DropdownMenuItem>
+                </a>
+
+                {/* My post Page*/}
+                <a href="/dashboard/myposts" >
+                  <DropdownMenuItem className="cursor-pointer" >
+                    <Rss />
+                    My Posts
+                  </DropdownMenuItem>
+                </a>
+
+                {/* Chat Page*/}
+                <a href="/dashboard/chats" >
+                  <DropdownMenuItem className="cursor-pointer" >
+                    <MessageCircleMore />
+                    Chats
+                  </DropdownMenuItem>
+                </a>
+
+                {/* Notifications Page whrere user get reuqest*/}
+                <DropdownMenuItem>
+                  <Bell />
+                  Notifications
                 </DropdownMenuItem>
-              </a>
-
-              {/* My post Page*/}
-              <a href="/dashboard/myposts" >
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <button onClick={handleSignOut} className="w-full text-left">
                 <DropdownMenuItem className="cursor-pointer" >
-                  <Rss />
-                  My Posts
+                  <LogOut />
+                  Log out
                 </DropdownMenuItem>
-              </a>
-
-              {/* Chat Page*/}
-              <a href="/dashboard/chats" >
-                <DropdownMenuItem className="cursor-pointer" >
-                  <MessageCircleMore />
-                  Chats
-                </DropdownMenuItem>
-              </a>
-
-              {/* Notifications Page whrere user get reuqest*/}
-              <DropdownMenuItem>
-                <Bell />
-                Notifications
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <button onClick={handleSignOut} className="w-full text-left">
-              <DropdownMenuItem className="cursor-pointer" >
-                <LogOut />
-                Log out
-              </DropdownMenuItem>
-            </button>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </SidebarMenuItem>
-    </SidebarMenu >
-  )
-}
+              </button>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </SidebarMenuItem>
+      </SidebarMenu >
+    )
+  }
