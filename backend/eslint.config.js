f46749288e70248a89fdd1430c38999b5567b1af
Supachai Ruknuy <supachai5028@gmail.com>
2025-06-20 14:14:04 -0400

    set-up chat-page

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -8,10 +8,10 @@ import { defineConfig } from "eslint/config";
 // Resolve conflict: use the lastes config
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
   { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
   { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
-  { files: ["**/*.test.js", "**/__tests__/**/*.js"], plugins: { jest }, languageOptions: { globals: globals.jest, }, rules: { },},
+  { files: ["**/*.test.js", "**/__tests__/**/*.js"], plugins: { jest }, languageOptions: { globals: globals.jest, }, rules: {}, },
 
 ]);
